f61968937f99daa0ae350c6d4d9f9bfd60b44a37
Dipesh <dipeshghimire.dg@gmail.com>
2023-04-02 12:59:42 +0545

    added go-to top button in Homepage

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,21 @@
-(()=>{
+(() => {
+   // Navbar
    const toggleButton = document.getElementById("toggle-button");
    const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-   toggleButton.addEventListener("click",()=>{
+   toggleButton.addEventListener("click", () => {
       navbarLinks.classList.toggle("active");
+   })
+})();
+
+
+(() => {
+   // Scroll to top
+   const toTop = document.querySelector(".to-top");
+   window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 100) {
+         toTop.classList.add("active")
+      } else {
+         toTop.classList.remove("active")
+      }
    })
 })();
